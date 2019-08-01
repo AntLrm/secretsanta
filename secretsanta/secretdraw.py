a3e05157623fa24222f0c2bba5944713dd3e42da
Antoine LORMEAU <antoine.lormeau@bureauveritas.com>
2019-08-01 18:04:26 +0200

    Simplified search algorithm

--- secretsanta/secretdraw.py
+++ secretsanta/secretdraw.py
@@ -45,59 +45,29 @@ class secretdraw():
         """
         set self.roll from a roll file.
         """
-	
 
     def mroll(self):
         """
         Launch a draw, and select randomly a solution in solution list.
         """
-        self.compute_path_list()
-        self.roll = random.choice(self.path_list)
+        self.roll = self.getrandom_path()
 
-    def compute_path_list(self):
-        """
-        Compute list of all possible solutions.
-        """
+    def getrandom_path(self):
         people_list = list(self.people.keys())
-        gift_tree, maxdepth = self.get_tree(people_list[0], people_list[1:])
-
-        if maxdepth == len(people_list):
-            self.path_list = gift_tree.get_maxpaths_list_from_tree()
-        else:
-            print('No solution found. Your secret santa may be too constrained')
-            sys.exit
-
-    def get_tree(self, top_node, other_node_list):
-        """
-        Return a tree of solutions from a top node and a list of people.
-        """
-        output_tree = tree()
-        output_tree.set_mainnode(top_node)
-        max_subtree_depth = 0 
-        children_list = self.get_children(top_node, other_node_list)
-
-        if len(children_list) == 0 :
-            return output_tree, max_subtree_depth + 1
-        else :
-            for child in children_list :
-                reduced_node_list = other_node_list[:]
-                reduced_node_list.remove(child)
-                subtree, subtree_depth = self.get_tree(child, reduced_node_list)
-                output_tree.addsubtree(subtree)
-                if subtree_depth > max_subtree_depth:
-                    max_subtree_depth = subtree_depth
-            return output_tree, max_subtree_depth + 1 
-    
-    def get_children(self, top_node, candidates):
-        """
-        Get all possible people to offer a gift.
-        """
-        children_list = []
-        for name in candidates:
-            if not (name in self.constrain[top_node]):
-                children_list.append(name)
-        return children_list
-
+        path = [] 
+        while len(path) < len(people_list):
+            path = []
+            people_not_in_path = people_list[:]
+            available_names = people_list[:]
+            while len(available_names) > 0:
+                next_name = random.choice(available_names)
+                path.append(next_name)
+                people_not_in_path.remove(next_name)
+                available_names = people_not_in_path[:]
+                for constrain in self.constrain[next_name]:
+                    if constrain in available_names:
+                        available_names.remove(constrain)
+        return path
 
     def addconstrains(self, constrain_list):
         """
@@ -129,51 +99,3 @@ class secretdraw():
         except IOError:
             print('Error trying to open: ' + file_path + '. Please check existence.')
             sys.exit()
-    
-class tree():
-    """
-    Tree object.
-    - mainnode : top node of the tree
-    - subtree_list : list of all tree object that are child of the mainnode.
-    """
-    def __init__(self):
-        self.mainnode = ''
-        self.subtree_list = []
-
-    def set_mainnode(self, mainnode_to_set):
-        self.mainnode = mainnode_to_set
-
-    def addsubtree(self, subtree_to_add):
-        self.subtree_list.append(subtree_to_add)
-
-    def get_maxpaths_list_from_tree(self):
-        """
-        Return the list of all paths that have the maximum lenght possible for this tree.
-        """
-        maxpaths_list = []
-        if self.subtree_list == []:
-            return [[self.mainnode]]
-
-        subtree_maxdepth = 1
-        for tree in self.subtree_list:
-            subtree_maxpaths_list = tree.get_maxpaths_list_from_tree()
-            #pdb.set_trace()
-            if len(subtree_maxpaths_list[0]) >= subtree_maxdepth:
-                if len(subtree_maxpaths_list[0]) > subtree_maxdepth:
-                    subtree_maxdepth = len(subtree_maxpaths_list[0])
-                    maxpaths_list = []
-                maxpaths_list.extend(self.add_item_to_all_list(self.mainnode, subtree_maxpaths_list))
-
-        return maxpaths_list
-
-    def add_item_to_all_list(self, item_to_add, list_of_list):
-        """
-        add an item at first position of all sub lists in a list of lists.
-        """
-        list_of_list_to_return = []
-        for mlist in list_of_list: 
-            new_list = [item_to_add]
-            new_list.extend(mlist)
-            list_of_list_to_return.append(new_list)
-        return list_of_list_to_return
-
